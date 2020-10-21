5db4acc96e52ca7f1f6e80d55ff60054a4450df3
Adrian <Adrian>
2020-10-21 09:50:43 +0200

    credentials (or read from your own source)

---
+++ scripts/gaming/tenjin_dv_to_bq/credential.py
@@ -0,0 +1,9 @@
+import os
+
+
+default_redshift_conn = {'host':os.environ['HOST123'],
+                 'port':'5439',
+                 'dbname':'***',
+                 'user':'***',
+                 'password': os.environ['PASS123']
+                 }
